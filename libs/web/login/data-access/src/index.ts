7133f7d334d293f0493b0abe98ee6ccd23ec27aa
Greenhair <aukeswen2001@yahoo.com>
2022-02-21 22:17:29 +0100

    add auth service lib and auth guard

---
+++ libs/web/login/data-access/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/web-login-data-access.module';
---
+++ libs/web/login/feature/src/lib/guard/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from 'libs/web/login/data-access/src/lib/auth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    return this.canActivate(childRoute, state);
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): true | UrlTree {
+      const url: string = state.url;
+
+      return this.checkLogin(url);
+  }
+
+  checkLogin(url: string): true|UrlTree {
+    if (this.authService.isLoggedIn) { 
+      if(url == '/') {
+        return this.router.parseUrl('dashboard')
+      }
+      return true; 
+    }
+
+    // Store the attempted URL for redirecting
+    this.authService.redirectUrl = url
+
+    // Redirect to the login page
+    return this.router.parseUrl('/login');
+  }
+  
+}
