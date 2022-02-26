acebfdcb1cff2cc6202fcdbd17cb04d92aeb1d82
Greenhair <aukeswen2001@yahoo.com>
2022-02-26 17:56:05 +0100

    move auth guard into library

--- apps/my-app/src/app/components/main/main-routing.module.ts
+++ apps/my-app/src/app/components/main/main-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../../guards/auth/auth.guard';
+import { AuthGuard } from '@my-app/web/login/guard';
 import { MainComponent } from './main.component';
 
 const routes: Routes = [
