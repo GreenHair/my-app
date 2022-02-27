128c151d29003589835884d9310d72780f221d9e
Greenhair <aukeswen2001@yahoo.com>
2022-02-27 10:00:42 +0100

    Dark navbar and logout button

--- libs/web/main/feature/src/lib/main.component.ts
+++ libs/web/main/feature/src/lib/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Credentials } from 'libs/shared/util/dto/src/lib/credentials.dto';
 import { LocalStorageService } from 'libs/web/shared/local-storage-service/src/lib/local-storage.service';
 
@@ -10,16 +10,23 @@ import { LocalStorageService } from 'libs/web/shared/local-storage-service/src/l
 })
 export class MainComponent implements OnInit {
 
+  isMenuCollapsed: boolean = true
   user: Credentials | null = null
   menu = [
     {title: "Dashboard", fragment: "dashboard"},
     {title: "Invoice", fragment: "invoice"},
   ]
 
-  constructor(private localStorage: LocalStorageService, public route: ActivatedRoute) { }
+  constructor(private localStorage: LocalStorageService, public route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.user = this.localStorage.get<Credentials>('user')
+  }
+
+  logout() {
+    this.localStorage.set('user', '')
+    this.localStorage.set('isLoggedIn', JSON.stringify(false))
+    this.router.navigateByUrl('login')
   }
 
 }
