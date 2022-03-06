551180607ef367136cefcabf53b5f13136fa73ec
Greenhair <aukeswen2001@yahoo.com>
2022-03-06 16:29:31 +0100

    http interceptor barrel file

--- apps/my-app/src/app/app.module.ts
+++ apps/my-app/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthInterceptor } from 'libs/web/shared/interceptor/src/lib/auth.interceptor'
+import { httpInterceptorProviders } from '@my-app/web/shared/interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import { AuthInterceptor } from 'libs/web/shared/interceptor/src/lib/auth.interc
     HttpClientModule,
     NgbModule,
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- libs/web/shared/interceptor/src/index.ts
+++ libs/web/shared/interceptor/src/index.ts
@@ -1,1 +1,9 @@
-export * from './lib/web-shared-interceptor.module';
+/* "Barrel" of Http Interceptors */
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptor } from './lib/auth.interceptor';
+
+/** Http interceptor providers in outside-in order */
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+];
