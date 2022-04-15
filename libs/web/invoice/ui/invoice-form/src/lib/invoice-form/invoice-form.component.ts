262045d24406bb230609a724cda1aefe083a2070
Greenhair <aukeswen2001@yahoo.com>
2022-04-16 00:02:15 +0200

    add input and output

--- libs/web/invoice/ui/invoice-form/src/lib/invoice-form/invoice-form.component.ts
+++ libs/web/invoice/ui/invoice-form/src/lib/invoice-form/invoice-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Invoice } from 'libs/web/invoice/data-access/src/lib/invoice';
 
 @Component({
   selector: 'my-app-invoice-form',
   templateUrl: './invoice-form.component.html',
   styleUrls: ['./invoice-form.component.css']
 })
-export class InvoiceFormComponent implements OnInit {
+export class InvoiceFormComponent implements OnChanges {
+
+  @Input() invoice: Invoice
+  @Output() onSubmit: () => {}
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("on changes", changes)
   }
+
 
 }
