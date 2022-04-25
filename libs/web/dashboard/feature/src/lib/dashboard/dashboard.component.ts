31c87bc264c84cc464c8899bffc4dd318679ffd9
Greenhair <aukeswen2001@yahoo.com>
2022-04-25 22:31:28 +0200

    refactor dashboard

--- libs/web/dashboard/feature/src/lib/dashboard/dashboard.component.ts
+++ libs/web/dashboard/feature/src/lib/dashboard/dashboard.component.ts
@@ -1,27 +1,26 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { EinkommenDto } from '@my-app/shared/util/dto';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { IncomeService } from '@my-app/web/income/data-access';
 import { Invoice, InvoiceService } from '@my-app/web/invoice/data-access';
 import { SumPipe } from '@my-app/web/shared/utils';
-import { Einkommen } from 'libs/api/data-access/entities/src/lib/Einkommen';
 import { IPeriodQuery, YearMonthWeekComponent } from 'libs/web/shared/ui/src/lib/year-month-week/year-month-week.component';
 import * as moment from 'moment';
-import { map, Observable, Subscription, switchMap, tap } from 'rxjs';
+import { map, Observable, Subject, Subscription, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
+export class DashboardComponent implements OnInit, OnDestroy {
 
   @ViewChild('periodSelection') periodSelection: YearMonthWeekComponent
   invoices$: Observable<Invoice[]>
   years$: Observable<number[]>
+  periodSelectionChanged$ = new Subject()
   incomeSubscription: Subscription
   sumOut: number = 0
   sumIn: number = 0
-  initialValues : IPeriodQuery = {
+  initialValues: IPeriodQuery = {
     period: 'month',
     query: {
       year: new Date().getFullYear(),
@@ -30,11 +29,27 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  constructor(private invoiceService: InvoiceService, 
+  constructor(private invoiceService: InvoiceService,
     private incomeService: IncomeService, private sumPipe: SumPipe) { }
 
   ngOnInit(): void {
-    
+    this.invoices$ = this.periodSelectionChanged$.pipe(
+      map(formValue => {
+        const periodQuery = formValue as IPeriodQuery
+        let query: { year: number, month?: number, week?: number } = { year: periodQuery.query.year }
+        switch (periodQuery.period) {
+          case 'month': query = { ...query, month: periodQuery.query.month }; break;
+          case 'week': query = { ...query, week: periodQuery.query.week }; break;
+        }
+        return query
+      }),
+      switchMap(query => this.invoiceService.getInvoices(query)),
+      tap(invoices => this.sumOut = this.sumPipe.transform(invoices)),
+      map(invoices => {
+        console.log("invoices", invoices)
+        return invoices.sort((a, b) => b.date.getTime() - a.date.getTime())
+      })
+    )
 
     this.incomeSubscription = this.incomeService.getIncomeList().subscribe(
       income => {
@@ -45,38 +60,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     this.years$ = this.invoiceService.getYears()
   }
 
-  ngAfterViewInit(): void {
-    /* this.invoices$ = this.periodSelection.valueChanged$.pipe(
-      switchMap(form => {
-        console.log(form)
-        return this.invoiceService.getInvoices()
-      }),
-      tap(invoices => this.sumOut = this.sumPipe.transform(invoices)),
-    ) */
-  }
-
-  onPeriodChanged(formValue: IPeriodQuery) {
-    console.log(formValue)
-    const periodQuery = formValue as IPeriodQuery
-    switch (periodQuery.period) {
-      case 'all': 
-        delete periodQuery.query.month;
-        delete periodQuery.query.week;
-        break;
-      case 'month': delete periodQuery.query.week; break;
-      case 'week': delete periodQuery.query.month; break;
-    }
-    this.invoices$ = this.invoiceService.getInvoices(periodQuery.query).pipe(
-      tap(invoices => this.sumOut = this.sumPipe.transform(invoices)),
-      map(invoices => {
-        console.log("invoices", invoices)
-        return invoices.sort((a, b) => b.date.getTime() - a.date.getTime())
-      })
-    )
-  }
-
-
   ngOnDestroy(): void {
-      this.incomeSubscription.unsubscribe()
+    this.incomeSubscription.unsubscribe()
   }
 }
