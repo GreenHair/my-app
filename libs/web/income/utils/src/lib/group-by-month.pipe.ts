b01177c7b33be2af62065b4ce721a5d3fa205707
Greenhair <aukeswen2001@yahoo.com>
2022-05-01 08:26:23 +0200

    add parameter for order

--- libs/web/income/utils/src/lib/group-by-month.pipe.ts
+++ libs/web/income/utils/src/lib/group-by-month.pipe.ts
@@ -6,12 +6,13 @@ import { EinkommenDto } from '@my-app/shared/util/dto';
   name: 'groupByMonth',
 })
 export class GroupByMonthPipe implements PipeTransform {
-  transform(value: EinkommenDto[]): EinkommenDto[][] {
+  transform(value: EinkommenDto[], order = 'asc'): EinkommenDto[][] {
     const temp = []
     for(let i = 0; i < 12; i++) {
       temp[i] = value?.filter(e => new Date(e.datum).getMonth() == i) ?? []
     }
-    return temp.filter(e => e.length > 0)
+    const ret = temp.filter(e => e.length > 0)
+    return order.toLocaleLowerCase() == 'desc' ? ret.reverse() : ret
   }
 }
 
