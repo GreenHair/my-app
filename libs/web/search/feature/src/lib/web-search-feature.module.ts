ff0795282657811861cb51c8b8ab439d6235a2f3
Greenhair <aukeswen2001@yahoo.com>
2022-05-07 11:55:06 +0200

    add search-form library

--- libs/web/search/feature/src/lib/web-search-feature.module.ts
+++ libs/web/search/feature/src/lib/web-search-feature.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SearchComponent } from './search.component';
+import { WebSearchUiModule } from '@my-app/web/search/ui';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    WebSearchUiModule,
     RouterModule.forChild([
       {path: '', pathMatch: 'full', component: SearchComponent}
     ]),
