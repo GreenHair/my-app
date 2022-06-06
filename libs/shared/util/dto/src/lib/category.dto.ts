7281420912e5eb5e7131f07015928d613c285708
Greenhair <aukeswen2001@yahoo.com>
2022-06-06 21:11:46 +0200

    updte category dto

--- libs/shared/util/dto/src/lib/category.dto.ts
+++ libs/shared/util/dto/src/lib/category.dto.ts
@@ -5,6 +5,6 @@ export class CategoryDto {
     id: number
     @IsNotEmpty()
     bezeichnung: string
-    @IsIn([0,1])
+    //@IsIn([0,1]) // unnecessary, automatic conversionfrom boolean to 0 or 1
     essen: boolean
 }
