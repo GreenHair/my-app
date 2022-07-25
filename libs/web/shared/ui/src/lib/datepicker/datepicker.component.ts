01b52b107f2cbf5b07f545d992c7d5c44dd28fc5
Greenhair <aukeswen2001@yahoo.com>
2022-07-25 15:15:50 +0200

    add datepicker component

---
+++ libs/web/shared/ui/src/lib/datepicker/datepicker.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { CustomAdapter } from '@my-app/web/invoice/utils';
+import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'my-app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: DatepickerComponent
+    },
+    {
+      provide: NgbDateAdapter,
+      useClass: CustomAdapter
+    }
+  ]
+})
+export class DatepickerComponent implements OnInit, ControlValueAccessor, OnDestroy {
+
+  date = new FormControl('');
+
+  private onchangeSub!: Subscription
+
+  onChange = (date: any) => {}
+  onTouched = () => {}
+
+  constructor() { }
+
+  writeValue(obj: any): void {
+    this.date.patchValue(obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  ngOnInit(): void {
+    this.onchangeSub = this.date.valueChanges.subscribe(val => this.onChange(val))
+  }
+
+  ngOnDestroy(): void {
+    this.onchangeSub.unsubscribe() 
+  }
+}
--- libs/web/shared/ui/src/lib/web-shared-ui.module.ts
+++ libs/web/shared/ui/src/lib/web-shared-ui.module.ts
@@ -4,21 +4,26 @@ import { YearMonthWeekComponent } from './year-month-week/year-month-week.compon
 import { ReactiveFormsModule } from '@angular/forms';
 import { DeleteButtonComponent } from './delete-button/delete-button.component';
 import { EditButtonComponent } from './edit-button/edit-button.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    NgbModule,
   ],
   declarations: [
     YearMonthWeekComponent,
     DeleteButtonComponent,
-    EditButtonComponent
+    EditButtonComponent,
+    DatepickerComponent
   ],
   exports: [
     YearMonthWeekComponent,
     DeleteButtonComponent,
-    EditButtonComponent
+    EditButtonComponent,
+    DatepickerComponent
   ],
 })
 export class WebSharedUiModule {}
